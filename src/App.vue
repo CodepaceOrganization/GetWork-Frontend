b248438752a5e0c4d12bd752d462affc642b79e8
Jeremy Joel Quispe Andia <u202216279@upc.edu.pe>
2024-04-24 22:00:08 -0500

    fix: delete the content on the components.

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,14 @@
-<script setup>
+<script>
+import FooterContent from "/src/public/components/footer-content.component.vue"
+
+export default{
+name: 'Main',
+components: {FooterContent},
+}
 </script>
 
 <template>
+ <footer-content></footer-content>
 </template>
 
 <style scoped>
--- src/public/components/footer-content.component.vue
+++ src/public/components/footer-content.component.vue
@@ -1,11 +1,15 @@
-<script setup>
-
+<script>
+export default {
+  name: "footer-content"
+}
 </script>
 
 <template>
-
+  <div class="grid bg-primary mt-4 p-4 align-content-start">
+    <div class="col-12 ml-3 align-items-center justify-content-center">
+      <p>Copyright &copy; 2024. ACME Studios</p>
+    </div>
+    {{ $t('authoring-phrase.intro') }}
+    {{ $t('authoring-phrase.author') }}
+  </div>
 </template>
-
-<style scoped>
-
-</style>
