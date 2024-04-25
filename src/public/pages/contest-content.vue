e974e09f0538c982272b8b57363ec321ad533a77
Alexander Jair Castillo <142842509+U202211390@users.noreply.github.com>
2024-04-25 18:32:45 -0500

    feat: added style and images of contest figma.

--- src/public/pages/contest-content.vue
+++ src/public/pages/contest-content.vue
@@ -6,19 +6,66 @@ export default {
 </script>
 
 <template>
-  <pv-card style="width: 25rem; overflow: hidden">
-    <template #header>
-      <img alt="user header" src="https://primefaces.org/cdn/primevue/images/usercard.png" />
+
+  <div class="ImageContainer">
+    <img alt="user header" src="https://github.com/CodepaceOrganization/GetWork-Frontend/assets/142842509/b4ce5e39-1ee8-4e16-8d93-c84248ca8cec" />
+    <p class="getworkContainer">
+      GetWork Contest
+    </p>
+  </div>
+
+
+  <pv-card style="width: 25rem; overflow: hidden" >
+    <template #header class="card-image">
+      <img alt="user header" src="https://github.com/CodepaceOrganization/GetWork-Frontend/assets/142842509/37dbe0d4-c7d1-4608-bf35-f9f6eebc568f" />
     </template>
-    <template #title>Advanced Card</template>
-    <template #subtitle>Card subtitle</template>
+
+    <template #title class="pv-cardText">
+      Weekly Contest 392
+    </template>
+
+    <template #subtitle class="pv-cardSubtitle">
+      Apr 6 2024 8:30 pm
+    </template>
+
     <template #content>
-      <p class="m-0">
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore sed consequuntur error repudiandae numquam deserunt quisquam repellat libero asperiores earum nam nobis, culpa ratione quam perferendis esse, cupiditate neque
-        quas!
-      </p>
+
     </template>
   </pv-card>
 </template>
 
+<style>
+.ImageContainer {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: relative;
+  background-color: #E4E4E4;
+}
+
+.getworkContainer {
+  position: absolute;
+  bottom: 10px; /* Coloca el texto en la parte inferior del contenedor de la imagen */
+  left: 50%; /* Centra el texto horizontalmente */
+  transform: translateX(-50%); /* Ajusta la posición horizontal para centrar el texto */
+  font-size: 16px;
+  color: #FFF; /* Cambia el color del texto */
+  background-color: rgba(0, 0, 0, 0.5); /* Añade un fondo semi-transparente al texto */
+  padding: 5px 10px; /* Añade un poco de espacio interno alrededor del texto */
+  border-radius: 5px; /* Redondea las esquinas del contenedor de texto */
+}
+
+.card-image {
+  flex: 0 0 auto;
+  margin-right: 10px;
+}
+
+.pv-cardText {
+  margin-left: auto; /* Alinea el texto a la derecha */
+}
+
+.pv-cardSubtitle {
+  margin-left: auto; /* Alinea el texto a la derecha */
+}
+</style>
 
