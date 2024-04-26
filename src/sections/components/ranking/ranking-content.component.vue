751b511d516f3735127a015da6d2d03a87c878f2
Alexander Jair Castillo <142842509+U202211390@users.noreply.github.com>
2024-04-25 20:33:46 -0500

    change: locations folders of contest.

--- src/sections/components/ranking/ranking-content.component.vue
+++ src/sections/components/ranking/ranking-content.component.vue
@@ -5,9 +5,47 @@ export default {
 </script>
 
 <template>
+  <html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tu Página Web</title>
 
+  </head>
+  <body>
+
+  <div class="global-ranking">
+    <div class="sidebar">
+      <img src="avatar1.jpg" class="avatar" alt="Avatar 1">
+      <img src="avatar2.jpg" class="avatar" alt="Avatar 2">
+      <!-- Añade más imágenes según sea necesario -->
+    </div>
+    <div class="ranking-content">
+      <h2>Global Ranking</h2>
+      <ol id="ranking-list">
+        <!-- Los elementos del ranking se añadirán dinámicamente aquí -->
+      </ol>
+    </div>
+  </div>
+
+  <script src="script.js"></script>
+
+  </body>
+  </html>
 </template>
 
 <style scoped>
+
+.sidebar {
+  float: left;
+  margin-right: 20px;
+}
+
+.avatar {
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+  margin-bottom: 10px;
+}
 
 </style>
