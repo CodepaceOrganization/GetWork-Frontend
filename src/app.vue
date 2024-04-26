1b2193059e7efbffa73f06adcdae4439c9d93cce
Jeremy Joel Quispe Andia <u202216279@upc.edu.pe>
2024-04-26 11:38:18 -0500

    feat: added seection "Simulacrum"

--- src/app.vue
+++ src/app.vue
@@ -1,10 +1,13 @@
 <script>
 import FooterContent from "/src/public/components/footer-content.component.vue"
 import HeaderContent from "@/public/components/header-content.component.vue";
+import SimulacrumComponent from "@/sections/components/simulacrum.component.vue";
+import Simulacrum from "@/sections/components/simulacrum.component.vue";
+import SectionSimulacrum from "@/sections/components/simulacrum.component.vue";
 
 export default{
 name: 'Main',
-components: {HeaderContent, FooterContent},
+components: {SectionSimulacrum, Simulacrum, HeaderContent, FooterContent, SimulacrumComponent},
 }
 </script>
 
