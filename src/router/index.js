a5ddeaa8ab84e7422461a508011973d374190dab
Jeremy Joel Quispe Andia <u202216279@upc.edu.pe>
2024-04-28 13:20:04 -0500

    feat: added routes an sections

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,9 @@
 import {createRouter, createWebHistory} from "vue-router";
 import SimulacrumComponent from "@/sections/components/simulacrum.component.vue";
 import ContestListComponent from "@/contest/components/contest-list.component.vue";
+import plansComponent from "@/sections/components/plans.component.vue";
+import profileManagementComponent from "@/sections/components/profile-management.component.vue";
+import tutorsComponent from "@/sections/components/tutors.component.vue";
 
 
 
@@ -11,15 +14,20 @@ const router = createRouter({
     routes: [
         { path: '/simulacrum', component: SimulacrumComponent, meta: { title: 'Simulacrum' } },
         { path: '/contest-list', component: ContestListComponent, meta: { title: 'Contest' } },
-        { path: '/',          redirect: '/home' }
+        { path: '/Premium', component: plansComponent, meta: { title: 'Premium' } },
+        { path: '/User', component: profileManagementComponent, meta: { title: 'Profile Management' } },
+        { path: '/Tutors', component: tutorsComponent, meta: { title: 'Tutors' } },
+        { path: '/', redirect: '/home' }
     ]
 });
+
 
 router.beforeEach((to, from, next) => {
     let baseTitle = 'GetWork';
     document.title = `${ baseTitle } | ${to.meta["title"]}`;
     next();
 });
+
 export default router;
 
 
