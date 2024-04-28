468d0bf12c8ad13de96c7b52a4ae1f54d5302a8d
EliasYP <u202211817@upc.edu.pe>
2024-04-27 23:36:27 -0500

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -2,12 +2,15 @@
 
 import {createRouter, createWebHistory} from "vue-router";
 import SimulacrumComponent from "@/sections/components/simulacrum.component.vue";
+import ContestListComponent from "@/contest/components/contest-list.component.vue";
+
 
 
 const router = createRouter({
     history: createWebHistory(),
     routes: [
         { path: '/simulacrum', component: SimulacrumComponent, meta: { title: 'Simulacrum' } },
+        { path: '/contest-list', component: ContestListComponent, meta: { title: 'Contest' } },
         { path: '/',          redirect: '/home' }
     ]
 });
