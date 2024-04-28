0429107493f7a170776bd62fa04ff1de94c14daa
EliasYP <u202211817@upc.edu.pe>
2024-04-27 23:27:05 -0500

    feat: add section contest

--- src/router/index.js
+++ src/router/index.js
@@ -2,13 +2,11 @@
 
 import {createRouter, createWebHistory} from "vue-router";
 import SimulacrumComponent from "@/sections/components/simulacrum.component.vue";
-import ContestListComponent from "@/contest/components/contest-list.component.vue";
 
 
 const router = createRouter({
     history: createWebHistory(),
     routes: [
-        { path: '/contest-list', component: ContestListComponent, meta: { title: 'Contest' } },
         { path: '/simulacrum', component: SimulacrumComponent, meta: { title: 'Simulacrum' } },
     ]
 });
