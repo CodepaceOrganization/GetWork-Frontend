4cb33ceab993cab2977e1d308dfacddec121b2b7
EliasYP <u202211817@upc.edu.pe>
2024-04-30 01:19:38 -0500

    feat: added the toolbar and applicate responsive

--- src/public/components/header-content.component.vue
+++ src/public/components/header-content.component.vue
@@ -1,25 +1,68 @@
-<script>
+<template>
+  <div>
+    <pv-toolbar class="toolbar" fixed>
+      <template #start>
+        <button @click="toggleDrawer" class="menu-button">
+          ☰
+        </button>
+        <pv-avatar image="https://github.com/CodepaceOrganization/Informes/assets/142842509/851f5611-99a5-4c48-af26-87346213f56e" style="width: 100px; height: 40px"/>
+        <div class="flex-row">
+          <router-link v-for="(item, index) in items" :key="item.label" v-slot="{ navigate, href }" :to="item.to" custom>
+            <pv-button :href="href" class="p-button-text" :class="item.class" :style="{ fontSize: isDrawerOpen ? '16px' : '' }" @click="navigate">
+              {{ item.label }}
+            </pv-button>
+          </router-link>
+        </div>
+        <div class="flex-row">
+          <router-link v-for="(item, index) in itemsRight" :key="item.label" v-slot="{ navigate, href }" :to="item.to" custom>
+            <pv-button :href="href" class="p-button-text" :class="item.class" :style="{ fontSize: isDrawerOpen ? '16px' : '' }" @click="navigate">
+              {{ item.label }}
+            </pv-button>
+          </router-link>
+        </div>
+      </template>
+    </pv-toolbar>
 
-import {useRouter} from 'vue-router';
+    <div class="drawer" :class="{ open: drawer}">
+      <router-link v-for="(item, index) in items" :key="item.label" :to="item.to" @click="toggleDrawer">
+        <pv-button class="p-button-text" :class="item.class" :style="{ marginBottom: isDrawerOpen ? '1rem' : '' }">
+          {{ item.label }}
+        </pv-button>
+      </router-link>
+      <router-link v-for="(item, index) in itemsRight" :key="item.label" :to="item.to" @click="toggleDrawer">
+        <pv-button class="p-button-text" :class="item.class" :style="{ marginBottom: isDrawerOpen ? '1rem' : '' }">
+          {{ item.label }}
+        </pv-button>
+      </router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+import { useRouter } from 'vue-router';
 
 export default {
   name: "header-content",
   data() {
     return {
       drawer: false,
+      isDrawerOpen: false, // Agregamos una variable para rastrear si el drawer está abierto o cerrado
       items: [
-        {label: 'Problems', to: '/Problems', class: 'item-1'},
-        {label: 'Contest', to: '/Contest-list', class: 'item-2'},
-        {label: 'Simulacrum', to: '/Simulacrum', class: 'item-3'},
-        {label: 'Tutors', to: '/Tutors', class: 'item-4'},
-        {label: 'User', to: '/User', class: 'item-5'},
-        {label: 'Premium', to: '/Premium', class: 'item-6'},
+        { label: 'Technical Tests', to: '/technical-tests', class: 'item-1' },
+        { label: 'Contest', to: '/Contest-list', class: 'item-2' },
+        { label: 'Simulacrum', to: '/Simulacrum', class: 'item-3' },
+        { label: 'Tutors', to: '/Tutors', class: 'item-4' },
+      ],
+      itemsRight: [
+        { label: 'User', to: '/User', class: 'item-5' },
+        { label: 'Premium', to: '/Premium', class: 'item-6' },
       ]
     }
   },
   methods: {
     toggleDrawer() {
       this.drawer = !this.drawer;
+      this.isDrawerOpen = !this.isDrawerOpen; 
     },
     navigateTo(route) {
       const router = useRouter();
@@ -29,66 +72,98 @@ export default {
 }
 </script>
 
-<template>
-  <pv-toolbar class="toolbar" fixed>
-    <template #start>
-      <pv-avatar
-          image="https://github.com/CodepaceOrganization/Informes/assets/142842509/851f5611-99a5-4c48-af26-87346213f56e"
-          style="width: 62px; height: 32px"/>
-
-      <div class="flex-column">
-        <router-link v-for="item in items" :key="item.label" v-slot="{ navigate, href}" :to="item.to" custom>
-          <pv-button :href="href" class="p-button-text text-white" :class="item.class" @click="navigate">
-            {{ item.label }}
-          </pv-button>
-        </router-link>
-      </div>
-    </template>
-  </pv-toolbar>
-</template>
-
 <style scoped>
-
 * {
   margin: 0;
   padding: 0;
-  justify-content: center;
 }
 
 .toolbar {
   background-color: #BD3F57;
   color: white;
   padding: 1.2rem;
-  justify-content: center;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.menu-button {
+  background: none;
+  border: none;
+  color: white;
+  font-size: 24px;
+  cursor: pointer;
+  transition: transform 0.3s ease-in-out;
+  display: none; /* Ocultar el botón de menú por defecto */
+}
+
+.menu-button:hover {
+  transform: scale(1.2); /* Cambiamos el tamaño del botón al pasar el mouse */
+}
+
+.flex-row {
   display: flex;
 }
-.item-1{
-  margin-right: 3rem;
-  margin-left: 2rem;
+
+.flex-row .p-button-text {
+  margin-right: 5rem;
+  color: white;
 }
 
-.item-2{
-  margin-right: 3rem;
+.item-5 {
+  color: white !important;
+  margin-right: 1rem; /* Ajustar el margen entre los elementos */
 }
 
-.item-3{
-  margin-right: 3rem;
+.item-6 {
+  color: white !important;
 }
 
-.item-4{
-  margin-right: 3rem;
+.drawer {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 40%;
+  height: 40%;
+  margin-top: 4rem;
+  background-color: rgba(189, 63, 87, 0.49);
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start; /* Alinea los elementos al inicio */
+  justify-content: center;
+  z-index: 300;
+  transition: transform 0.3s ease-in-out; /* Agregamos una transición al abrir y cerrar el drawer */
+  transform: translateX(-100%);
 }
 
-.item-5{
-  margin-left: 55rem;
+.drawer.open {
+  transform: translateX(0);
 }
 
-.item-6{
-  margin-left: 10rem;
-  margin-bottom: -0.5rem;
-  color: #BD3F57 !important;
-  background-color: #ffffff;
-  padding: 0.6rem;
+.p-button-text {
+  color: white !important;
 }
 
+.drawer pv-button {
+  margin-bottom: 1rem;
+}
+
+@media (max-width: 768px) {
+  .menu-button {
+    display: block; /* Mostrar el botón de menú en pantallas pequeñas */
+  }
+
+  .flex-row {
+    display: none; /* Ocultar los elementos de la barra de herramientas en pantallas pequeñas */
+  }
+
+  .drawer {
+    display: flex; /* Mostrar el menú desplegable en pantallas pequeñas */
+  }
+
+  /* Estilos específicos para el menú hamburguesa */
+  .p-button-text {
+    font-size: 24px;
+  }
+}
 </style>
