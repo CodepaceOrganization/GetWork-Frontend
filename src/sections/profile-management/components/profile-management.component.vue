71c29b861d9e100c054ded524961d67a3cb606e4
FlowerpowerA <130100844+FlowerpowerA@users.noreply.github.com>
2024-05-03 21:41:59 -0500

    Update profile-management.component.vue

--- src/sections/profile-management/components/profile-management.component.vue
+++ src/sections/profile-management/components/profile-management.component.vue
@@ -30,20 +30,20 @@ export default {
       <img src="../../../assets/images/profile.png" alt="profile" class="w-30rem  "/>
     </div>
     <div class="text-4xl flex flex-column flex-wrap justify-content-center align-content-center">
-      <h2> Nombre:  {{this.getFullName }}</h2>
+      <h2> Name:  {{this.getFullName }}</h2>
       <h2> Dni:  {{this.getDni}}</h2>
-      <h2> Telefono: {{this.getPhoneNumber}}</h2>
+      <h2> PhoneNumber: {{this.getPhoneNumber}}</h2>
     </div>
   </div>
   <div class="mt-6 mx-6 flex flex-column my-5">
-    <h1 > Tutorías reservadas:</h1>
+    <h1 > Reserved tutorials :</h1>
     <pv-card class="p-7 shadow-5 mt-6">
       <template #title>
 
       </template>
       <template #content>
         <div class="text-center">
-          <h1> No tienes reservas</h1>
+          <h1>You have no reservations</h1>
         </div>
       </template>
     </pv-card>
