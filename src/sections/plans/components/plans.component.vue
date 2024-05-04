ffa7a92b1e9f850347d7b1728c29927f1b1dae00
FlowerpowerA <130100844+FlowerpowerA@users.noreply.github.com>
2024-05-03 21:32:22 -0500

    Update plans.component.vue

--- src/sections/plans/components/plans.component.vue
+++ src/sections/plans/components/plans.component.vue
@@ -14,7 +14,7 @@ export default {
           <p class="text-md mt-3"> NORMAL </p>
           <div class="flex gap-2">
             <p> FREE </p>
-            <p class="text-sm mt-2"> /mes </p>
+            <p class="text-sm mt-2"> /month </p>
           </div>
           <hr/>
         </template>
@@ -22,31 +22,31 @@ export default {
         <template #content>
           <div class="flex gap-2">
             <p class="m-0 mt-4 text-sm">
-              Acceso limitado a preguntas frecuentes en entrevistas seleccionadas
+              Limited access to frequently asked questions in select interviews
             </p>
           </div>
           <div class="flex gap-2">
             <p class="m-0 mt-4 text-sm">
-              Ejercicios de codificación basicos
+              Basic coding exercises
             </p>
           </div>
           <div class="flex gap-2">
             <p class="m-0 mt-4 text-sm">
-              Tutoriales introductorios sobre temas comunes en entrevistas
+            Introductory tutorials on common interview topics
             </p>
           </div>
           <div class="flex justify-content-center align-content-center mt-3">
-            <pv-button class="px-6 p-2 border-round-xl border-transparent bg-pink-300 text-white shadow-5 font-bold"> Comenzar </pv-button>
+            <pv-button class="px-6 p-2 border-round-xl border-transparent bg-pink-300 text-white shadow-5 font-bold"> Start </pv-button>
           </div>
         </template>
       </pv-card>
 
       <pv-card class=" card w-19rem p-2  px-5 text-white border-round-xl  ">
         <template #title>
-          <p class="text-md mt-3"> ESTUDIANTE </p>
+          <p class="text-md mt-3"> STUDENT </p>
           <div class="flex gap-2">
             <p> S/35.00 </p>
-            <p class="text-sm mt-2"> /mes </p>
+            <p class="text-sm mt-2"> /month </p>
           </div>
           <hr/>
         </template>
@@ -54,34 +54,34 @@ export default {
         <template #content>
           <div class="flex gap-2">
             <p class="m-0 mt-4 text-sm">
-              Acceso completo a preguntes frecuentes de entrevistas
+            Full access to frequently asked interview questions
             </p>
           </div>
           <div class="flex gap-2">
             <p class="m-0 mt-4 text-sm">
-              Ejercicios de codificación práctica ilimitado
+              Unlimited Practice Coding Exercises
             </p>
           </div>
           <div class="flex gap-2">
             <p class="m-0 mt-4 text-sm">
-              Simulacro de entrevistas en vivo con expertos
+              Mock live interviews with experts
             </p>
           </div>
           <div class="flex gap-2">
-            <a href="../../../.." class="text-white"> <p class="m-0 mt-2 text-sm text-white "> Ver mas </p></a>
+            <a href="../../../.." class="text-white"> <p class="m-0 mt-2 text-sm text-white "> See more </p></a>
           </div>
           <div class="flex justify-content-center align-content-center mt-3">
-            <pv-button class="px-6 p-2 border-round-xl border-transparent bg-pink-300 text-white shadow-5 font-bold"> Comenzar </pv-button>
+            <pv-button class="px-6 p-2 border-round-xl border-transparent bg-pink-300 text-white shadow-5 font-bold"> Start </pv-button>
           </div>
         </template>
       </pv-card>
 
       <pv-card class=" card w-19rem p-2  px-5 text-white border-round-xl  ">
         <template #title>
-          <p class="text-md mt-3"> DESARROLLADOR </p>
+          <p class="text-md mt-3"> DEVELOPER </p>
           <div class="flex gap-2">
             <p> S/69.90 </p>
-            <p class="text-sm mt-2"> /mes </p>
+            <p class="text-sm mt-2"> /month </p>
           </div>
           <hr/>
         </template>
@@ -89,19 +89,19 @@ export default {
         <template #content>
           <div class="flex gap-2">
             <p class="m-0 mt-4 text-sm">
-              Acceso completo a todas las preguntas frecuentes de entrevistas
+             Full access to all frequently asked interview questions
             </p>
           </div>
           <div class="flex gap-2">
             <p class="m-0 mt-4 text-sm">
-              Ejercicios de codificación práctica avanzada
+            Advanced Practical Coding Exercises
             </p>
           </div>
           <div class="flex gap-2">
-            <a href="../../../.." class="text-white"> <p class="m-0 mt-2 text-sm text-white "> Ver mas </p></a>
+            <a href="../../../.." class="text-white"> <p class="m-0 mt-2 text-sm text-white "> See more </p></a>
           </div>
           <div class="flex justify-content-center align-content-center mt-3">
-            <pv-button class="px-6 p-2 border-round-xl border-transparent bg-pink-300 text-white shadow-5 font-bold"> Comenzar </pv-button>
+            <pv-button class="px-6 p-2 border-round-xl border-transparent bg-pink-300 text-white shadow-5 font-bold"> Start </pv-button>
           </div>
         </template>
       </pv-card>
