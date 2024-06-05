9d0692234e6f6cd150e1d0b8f84a4a6751f86b59
Sebastian Cachis <u202210846@upc.edu.pe>
2024-06-05 18:57:26 -0500

    style: added in the buttons to change the state

--- src/sections/technical-test/components/technical-test-detail-card.component.vue
+++ src/sections/technical-test/components/technical-test-detail-card.component.vue
@@ -54,22 +54,22 @@ export default {
     <template #content>
       <div class="container">
         <div class="column">
-            <div class="left-container">
-                <button v-if="technicalTestDetail.progress === 'InProgress'" @click="updateProgress('Earrings')"><i class="pi pi-chevron-left"></i></button>
-                <button v-if="technicalTestDetail.progress === 'Done'" @click="updateProgress('InProgress')"><i class="pi pi-chevron-left"></i></button>
-                <p>{{index}}. {{ technicalTestDetail.description }}</p>
-            </div>
-            <div v-if="technicalTestDetail.progress === 'Earrings'" class="right-container">
-                <p :class="technicalTestDetail.difficulty.toLowerCase()">{{ technicalTestDetail.difficulty }}</p>
-                <button v-if="technicalTestDetail.progress === 'Earrings'" @click="updateProgress('InProgress')"><i class="pi pi-chevron-right"></i></button>
-            </div>
-            <div v-else-if="technicalTestDetail.progress === 'InProgress'" class="right-container">
-                <p :class="getStatusColor(technicalTestDetail.status)">{{ technicalTestDetail.status }}/100</p>
-                <button v-if="technicalTestDetail.progress === 'InProgress'" @click="updateProgress('Done')"><i class="pi pi-chevron-right"></i></button>
-            </div>
-            <div v-else-if="technicalTestDetail.progress === 'Done'" class="right-container">
-                <p class="status-green">{{ technicalTestDetail.progress }}</p>
-            </div>
+          <div class="left-container">
+            <button v-if="technicalTestDetail.progress === 'InProgress'" @click="updateProgress('Earrings')"><i class="pi pi-chevron-left"></i></button>
+            <button v-else-if="technicalTestDetail.progress === 'Done'" @click="updateProgress('InProgress')"><i class="pi pi-chevron-left"></i></button>
+            <p>{{index}}. {{ technicalTestDetail.description }}</p>
+          </div>
+          <div v-if="technicalTestDetail.progress === 'Earrings'" class="right-container">
+            <p :class="technicalTestDetail.difficulty.toLowerCase()">{{ technicalTestDetail.difficulty }}</p>
+            <button @click="updateProgress('InProgress')"><i class="pi pi-chevron-right"></i></button>
+          </div>
+          <div v-else-if="technicalTestDetail.progress === 'InProgress'" class="right-container">
+            <p :class="getStatusColor(technicalTestDetail.status)">{{ technicalTestDetail.status }}/100</p>
+            <button @click="updateProgress('Done')"><i class="pi pi-chevron-right"></i></button>
+          </div>
+          <div v-else-if="technicalTestDetail.progress === 'Done'" class="right-container">
+            <p class="status-green">{{ technicalTestDetail.progress }}</p>
+          </div>
         </div>
       </div>
     </template>
@@ -111,8 +111,12 @@ export default {
 
 .left-container {
   flex: 0.75;
+  display: flex;
+  flex-direction: row; /* Cambia la dirección a fila */
+  align-items: center; /* Centra los elementos a lo largo del eje transversal */
+  justify-content: space-between; /* Añade espacio entre los elementos */
 }
-.left-container p{
+.left-container p {
   margin: 10px;
   line-height: 1.5;
 }
@@ -138,4 +142,11 @@ button {
     cursor: pointer;
     outline: inherit;
 }
+.pi-chevron-left, .pi-chevron-right {
+  /* Agrega aquí el estilo que desees para los iconos */
+  color: #000000; /* Cambia el color del icono */
+  font-size: 20px; /* Cambia el tamaño del icono */
+  margin: 0 10px; /* Agrega margen alrededor del icono */
+}
+
 </style>
