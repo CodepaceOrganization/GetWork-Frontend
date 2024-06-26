ddc87881d0dcd12837e5ea944922ed56bb297ab5
Alexander Jair Castillo <142842509+U202211390@users.noreply.github.com>
2024-06-26 17:42:06 -0500

    add: added endopint

--- src/sections/contest/services/courses-api.service.js
+++ src/sections/contest/services/courses-api.service.js
@@ -2,11 +2,11 @@ import http from "../../../shared/services/http-common.js";
 
 export class CoursesApiService{
     getAllCourses() {
-        return http.get('/contest');
+        return http.get('/weekly-contest');
     }
 
     getCourse(id) {
-        return http.get(`/contest/${id}`);
+        return http.get(`/weekly-contest/${id}`);
     }
 
     getRanking(){
